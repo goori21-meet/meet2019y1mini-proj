9f82056dfb34129d67028f9291a26a2c89a4e3c7
goori21-meet <goori21@meet.mit.edu>
2019-07-29 12:58:44 -0400

    Stopping here

--- lab_mini-proj.py
+++ lab_mini-proj.py
@@ -54,10 +54,121 @@ for i in range(START_LENGTH):
     #function to do this
     new_stamp()
 
+ 
+def remove_tail():
+    old_stamp = stamp_list.pop(0) # last piece of tail
+    snake.clearstamp(old_stamp) # erase last piece of tail
+    pos_list.pop(0) # remove last piece of tail's position
 
-turtle.mainloop() 
+
+    
+
+snake.direction = "Up"
+UP_EDGE = 250
+DOWN_EDGE = -250
+RIGHT_EDGE = 400
+LEFT_EDGE = -400
+
+def up():
+    snake.direction="Up" #Change direction to up
+    move_snake() #Update the snake drawing 
+    print("You pressed the up key!")
+
+#2. Make functions down(), left(), and right() that change snake.direction
+####WRITE YOUR CODE HERE!!
 
 
+def down():
+    snake.direction="Down" #Change direction to up
+     #Update the snake drawing 
+    print("You pressed the down key!")
+
+
+
+    
+def right():
+    snake.direction="Right" #Change direction to up
+    print("You pressed the right key!")
+
+
+
+def left():
+    snake.direction="Left" #Change direction to up 
+    print("You pressed the left key!")
+
+
+turtle.onkeypress(up, "Up") # Create listener for up key
+
+#3. Do the same for the other arrow keys
+####WRITE YOUR CODE HERE!!
+
+turtle.onkeypress(down,"Down")
+turtle.onkeypress(right, "Right")
+turtle.onkeypress(left, "Left")
+
+
+    
+turtle.listen()
+
+def move_snake():
+    my_pos = snake.pos()
+    x_pos = my_pos[0]
+    y_pos = my_pos[1]
+    new_pos = snake.pos()
+    new_x_pos = new_pos[0]
+    new_y_pos = new_pos[1]
+    if new_x_pos >= RIGHT_EDGE:
+         print("You hit the right edge! Game over!")
+         quit()
+    elif new_y_pos >= UP_EDGE:
+         print("You hit the up edge! Game over!")
+         quit()
+    elif new_y_pos <= DOWN_EDGE:
+         print("You hit the down edge! Game over!")
+         quit()             
+    elif new_x_pos <= LEFT_EDGE:
+         print("You hit the left edge! Game over!")
+         quit()
+         
+
+    #If snake.direction is up, then we want the snake to change
+    #it’s y position by SQUARE_SIZE
+    if snake.direction == "Up":
+        snake.goto(x_pos, y_pos + SQUARE_SIZE)
+        print("You moved up!")
+    elif snake.direction=="Down":
+        snake.goto(x_pos, y_pos - SQUARE_SIZE)
+        print("you moved down!")
+        
+    elif snake.direction=="Left":
+        snake.goto(x_pos- SQUARE_SIZE, y_pos)
+        print("you moved left!")
+        
+    elif snake.direction=="Right":
+        snake.goto(x_pos+SQUARE_SIZE, y_pos)
+        print("you moved right!")
+
+#Make the snake stamp a new square on the screen
+    #Hint - use a single function to do this
+    new_stamp()
+
+    ######## SPECIAL PLACE - Remember it for Part 5
+
+    #remove the last piece of the snake (Hint Functions are FUN!)
+    remove_tail()
+    
+
+        
+        
+    
+           
+
+
+
+
+
+
+    
 
 
 
